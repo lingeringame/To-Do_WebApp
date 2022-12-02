fffaef07442dadfaabf9bf535bdc5e088d8da69e
Sergio O <59296072+lingeringame@users.noreply.github.com>
2022-12-02 10:21:26 -0600

    Configuered application for MySQL

--- To-Do/To-Do/Startup.cs
+++ To-Do/To-Do/Startup.cs
@@ -28,7 +28,7 @@ namespace To_Do
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
